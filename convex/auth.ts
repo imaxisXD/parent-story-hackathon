f5d8cba8996a5fd7db3d67c493c6b51098b196cc
imaxisXD <sunny735084@gmail.com>
2025-10-01 17:56:57 +0530

    Remove redirectURI from GitHub authentication configuration in createAuth function to streamline the authentication process.

--- convex/auth.ts
+++ convex/auth.ts
@@ -25,7 +25,6 @@ export const createAuth = (
       github: {
         clientId: process.env.GITHUB_CLIENT_ID as string,
         clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-        redirectURI: `${siteUrl}/parent`,
       },
     },
 
